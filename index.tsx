9d152025d22767173e7fe97deba6d31ae365181b
Liran Baruch <liran.baruch@qlab-israel.com>
2023-09-04 11:15:08 +0300

    update repository files

--- index.tsx
+++ index.tsx
@@ -1,36 +1,35 @@
-import * as React from 'react'
+import * as React from "react";
 import ReCaptchaComponent from "./src/ReCaptchaComponent";
 
 export type IProps = {
-  captchaDomain: string
-  onReceiveToken: (captchaToken: string) => void
-  siteKey: string
-  action: string
-}
+  captchaDomain: string;
+  onReceiveToken: (captchaToken: string) => void;
+  siteKey: string;
+  action: string;
+};
 
-export type IState = {}
-
+export type IState = {};
 
 class ReCaptchaV3 extends React.PureComponent<IProps, IState> {
-
-  private _captchaRef: any
+  private _captchaRef: any;
 
   public refreshToken = () => {
-    this._captchaRef.refreshToken()
-  }
+    this._captchaRef.refreshToken();
+  };
 
   render() {
     return (
       <ReCaptchaComponent
-        ref={ref => this._captchaRef = ref}
+        ref={(ref) => (this._captchaRef = ref)}
         action={this.props.action}
         captchaDomain={this.props.captchaDomain}
         siteKey={this.props.siteKey}
         onReceiveToken={(token: string) => {
-          this.props.onReceiveToken(token)
-        }}/>
-    )
+          this.props.onReceiveToken(token);
+        }}
+      />
+    );
   }
 }
 
-export default ReCaptchaV3
+export default ReCaptchaV3;
